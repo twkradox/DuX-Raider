9103e168839b13cb65bf97763617fbacd5571503
kradox <adriandesouza87@gmail.com>
2026-07-20 08:33:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ def update_application(repo_owner, repo_name, current_version):
         pass
         
 
-repo_owner = 'Nyxoy201'
-repo_name = 'nebula'
+repo_owner = 'twkradox'
+repo_name = 'DuX-Raider'
 current_version = 'v1.3.2'
 
 update_application(repo_owner, repo_name, current_version)
